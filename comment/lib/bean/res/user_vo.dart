77d468f351549bdefc181ea7098f0eab09c427ba
uk <tuhaoailili@gmail.com>
2022-05-06 15:44:20 +0800

    改首页ui

--- comment/lib/bean/res/user_vo.dart
+++ comment/lib/bean/res/user_vo.dart
@@ -1,13 +1,118 @@
+/// age : 0
+/// cover : "https://img1.baidu.com/it/u=980679323,2277132941&fm=253&fmt=auto&app=138&f=JPEG?w=400&h=401"
+/// createdAt : "2022-01-26T16:41:22"
+/// deleted : 0
+/// fansNumber : 1
+/// gender : 0
+/// hasCoach : 0
+/// hasVip : 0
+/// hobby : ""
+/// id : 9
+/// introduction : ""
+/// label : ""
+/// nickName : "6"
+/// occupation : ""
+/// openId : ""
+/// phone : "18894720202"
+/// photoAlbumId : 0
+/// upNumber : 0
+/// updatedAt : "2022-02-10T16:34:11"
+/// updaterId : 7
+/// version : 17
+
 class UserVo {
-  String? nickName;
-
   UserVo({
-    this.nickName
-  });
+      this.age, 
+      this.cover, 
+      this.createdAt, 
+      this.deleted, 
+      this.fansNumber, 
+      this.gender, 
+      this.hasCoach, 
+      this.hasVip, 
+      this.hobby, 
+      this.id, 
+      this.introduction, 
+      this.label, 
+      this.nickName, 
+      this.occupation, 
+      this.openId, 
+      this.phone, 
+      this.photoAlbumId, 
+      this.upNumber, 
+      this.updatedAt, 
+      this.updaterId, 
+      this.version,});
 
-  factory UserVo.fromJson(json){
-    return UserVo(
-      nickName: json['nickName']
-    );
+  UserVo.fromJson(dynamic json) {
+    age = json['age'];
+    cover = json['cover'];
+    createdAt = json['createdAt'];
+    deleted = json['deleted'];
+    fansNumber = json['fansNumber'];
+    gender = json['gender'];
+    hasCoach = json['hasCoach'];
+    hasVip = json['hasVip'];
+    hobby = json['hobby'];
+    id = json['id'];
+    introduction = json['introduction'];
+    label = json['label'];
+    nickName = json['nickName'];
+    occupation = json['occupation'];
+    openId = json['openId'];
+    phone = json['phone'];
+    photoAlbumId = json['photoAlbumId'];
+    upNumber = json['upNumber'];
+    updatedAt = json['updatedAt'];
+    updaterId = json['updaterId'];
+    version = json['version'];
   }
+  int? age;
+  String? cover;
+  String? createdAt;
+  int? deleted;
+  int? fansNumber;
+  int? gender;
+  int? hasCoach;
+  int? hasVip;
+  String? hobby;
+  int? id;
+  String? introduction;
+  String? label;
+  String? nickName;
+  String? occupation;
+  String? openId;
+  String? phone;
+  int? photoAlbumId;
+  int? upNumber;
+  String? updatedAt;
+  int? updaterId;
+  int? version;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['age'] = age;
+    map['cover'] = cover;
+    map['createdAt'] = createdAt;
+    map['deleted'] = deleted;
+    map['fansNumber'] = fansNumber;
+    map['gender'] = gender;
+    map['hasCoach'] = hasCoach;
+    map['hasVip'] = hasVip;
+    map['hobby'] = hobby;
+    map['id'] = id;
+    map['introduction'] = introduction;
+    map['label'] = label;
+    map['nickName'] = nickName;
+    map['occupation'] = occupation;
+    map['openId'] = openId;
+    map['phone'] = phone;
+    map['photoAlbumId'] = photoAlbumId;
+    map['upNumber'] = upNumber;
+    map['updatedAt'] = updatedAt;
+    map['updaterId'] = updaterId;
+    map['version'] = version;
+    return map;
+  }
+
 }
--- lib/page/person/user_list_page.dart
+++ lib/page/person/user_list_page.dart
@@ -7,6 +7,7 @@ import 'package:httplib/io/http_proxy.dart';
 import 'package:provider/provider.dart';
 import 'package:uk_manager/provider/base_model.dart';
 import 'package:comment/bean/res/user_vo.dart';
+import 'package:uk_manager/widget/u_image.dart';
 
 import '../../widget/base_view.dart';
 
@@ -56,30 +57,75 @@ class UserListPage extends StatelessWidget {
                               model.loadData();
                             },child: const Text('搜索'),color: Colors.blue,textColor: Colors.white,)
                         ],
-                      )
+                      ),
+                      const Divider(height: 20,thickness: 1,color: Colors.blue,),
+
                     ],
                   );
                 },
                 selector: (context, model) => model.clickIndex),
 
-            Selector<_Model,int?>(builder: (context,value,child){
+            Row(
+              children:  const [
+                Expanded(child: Text('基本信息',style: TextStyle(color: Colors.blue),)),
+                Expanded(child: SizedBox()),
+                SizedBox(
+                  width: 200,
+                  child: Center(child: Text('注册日期',style: TextStyle(color: Colors.blue),)),
+                )
+              ],
+            ),
+            const SizedBox(height: 10,),
+
+            Expanded(child: Selector<_Model,int?>(builder: (context,value,child){
               var list = context.read<_Model>().userList;
               if(value==null || value.isNaN){
                 return const SizedBox();
               }
-              return ListView(
-                shrinkWrap: true,
-                children: List.generate(value, (index){
-                  var vo = list![index];
-                  return Row(
-                    children: [
-                      Text('${vo.nickName}')
-                    ],
-                  );
-                }),
-              );
+              return ListView.separated(
+                  shrinkWrap: true,
+                  itemBuilder: (context,index){
+                    var vo = list![index];
+                    return Row(
+                      children: [
+                        UImage('${vo.cover}',
+                          mWidth: 60,
+                          mHeight: 60,
+                        ),
+                        const SizedBox(width: 10,),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('${vo.nickName}',style: const TextStyle(fontSize: 18,fontWeight: FontWeight.bold,color: Colors.black),),
+                            const SizedBox(height: 5,),
+                            Text(vo.hasVip == 1?'VIP用户':'普通用户')
+                          ],
+                        ),
+                        const SizedBox(width: 10,),
+                        if(vo.hasCoach == 1)
+                          Container(
+                            child: const Text('私教认证√',style: TextStyle(color: Colors.white),),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                color: Colors.green
+                            ),
+                            padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 5),
+                            margin: const EdgeInsets.only(right: 10),
+                          ),
 
-            }, selector: (context,model)=>model.userList?.length)
+                        const Expanded(child: SizedBox()),
+                        SizedBox(width: 200,child: Center(child: Text('${vo.createdAt}'),),)
+
+
+                      ],
+                    );
+                  }, separatorBuilder: (context,index){
+                return const Divider(height: 20,thickness: 5,);
+              }, itemCount: value);
+
+            }, selector: (context,model)=>model.userList?.length)),
+
+
           ],
         ),
       ),
@@ -89,22 +135,24 @@ class UserListPage extends StatelessWidget {
 
 class _Model extends BaseModel {
   List<UserVo>? userList;
-  var parameters = {
-    'news':'',
+  var parameters = <String,dynamic>{
+    'news':false,
     'vip':false,
     'edu':false,
     'today':false,
   };
 
   var parList = [
-    _Vo('最新注册', 'news'),
-    _Vo('私教用户', 'vip'),
-    _Vo('vip用户', 'edu'),
-    _Vo('今日注册', 'today'),
+    _Vo('最新注册', 'news',value: 'news'),
+    _Vo('私教用户', 'edu',value: 'edu'),
+    _Vo('vip用户', 'vip',value: 'vip'),
+    _Vo('今日注册', 'today',value: 'today'),
   ];
   var clickIndex = 0;
 
-  _Model(BuildContext context) : super(context);
+  _Model(BuildContext context) : super(context){
+    loadData();
+  }
 
   void clickLabel(_Vo vo) {
     vo.check = !vo.check;
@@ -125,7 +173,9 @@ class _Model extends BaseModel {
         .then((value) {
       if (value.code==200) {
         var records = value.data['records'];
+        print('长度:${records.length}');
         List<UserVo> list=[];
+
         records.forEach((element) {
           list.add(UserVo.fromJson(element));
         });
